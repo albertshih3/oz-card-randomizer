025275f7f4bf31e3067b3f280a79acefab890a53
Albert Shih <albertshih3@gmail.com>
2026-03-24 22:54:02 -0700

    fix esm

--- api/users/invite.ts
+++ api/users/invite.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import { requireAuth, clerkClient } from "../_auth";
+import { requireAuth, clerkClient } from "../_auth.js";
 
 export default async function handler(
   req: VercelRequest,
